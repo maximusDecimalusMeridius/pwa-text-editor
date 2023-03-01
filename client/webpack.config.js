453d59167975ca591fe22dfffe794e029ff9ec73
maximusDecimalusMeridius <106376888+maximusDecimalusMeridius@users.noreply.github.com>
2023-03-01 09:59:47 -0800

    Began working through acceptance criteria and connected index.  Installed babel and css-loader, linking css and js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -14,16 +14,32 @@ module.exports = () => {
       install: './src/js/install.js'
     },
     output: {
-      filename: '[name].bundle.js',
+      filename: 'bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin'
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
       ],
     },
   };
