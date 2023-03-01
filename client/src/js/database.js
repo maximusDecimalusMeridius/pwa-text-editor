3c6b9c14654cd7718b7a12df0d186204ee0eb415
maximusDecimalusMeridius <106376888+maximusDecimalusMeridius@users.noreply.github.com>
2023-03-01 15:16:03 -0800

    Defined putDb method

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,19 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await openDB("jate", 1);
+  const tx = db.transaction("jate", "readwrite");
+  const store = tx.objecStore("jate");
+  const request = store.add({ text: content })
+  
+  const result = await request;
+  if(!result){
+    console.error('putDb not implemented');
+  } else {
+    console.log(`Saved!`, result);
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
