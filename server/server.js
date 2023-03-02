dd7f352a0c5fc2cefbc2933fe0123400c9e2ffd7
maximusDecimalusMeridius <106376888+maximusDecimalusMeridius@users.noreply.github.com>
2023-03-01 23:17:36 -0800

    Fixed path issues for manifest

--- server/server.js
+++ server/server.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use("/src", express.static("../client/src"));
 app.use(express.static('../client/dist'));
+// app.use("/dist", express.static("../client/dist"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
